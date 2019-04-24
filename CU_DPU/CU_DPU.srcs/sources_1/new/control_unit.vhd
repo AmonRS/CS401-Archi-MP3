11a1949682b4e672fe05040582c5985174328f99
Amon R Sthapit <amonsthapit@live.com>
2019-04-24 10:13:34 -0700

    adding CU and DPU

---
+++ CU_DPU/CU_DPU.srcs/sources_1/new/control_unit.vhd
@@ -0,0 +1,29 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity control_unit is
+    Port ( op:                 in  STD_LOGIC_VECTOR(7 downto 0);
+           zero:               in  STD_LOGIC;
+           memread, memwrite:  out STD_LOGIC;
+           pcsrc, alusrc:      out STD_LOGIC;
+           regdst, regwrite:   out STD_LOGIC;
+           jump:               out STD_LOGIC;
+           alucontrol:         out STD_LOGIC_VECTOR(3 downto 0));
+end control_unit;
+
+architecture Behavioral of control_unit is
+    component main_decoder
+    port ( op:                    in  STD_LOGIC_VECTOR(7 downto 0);
+           alusrc:              out STD_LOGIC;
+           jump:                out STD_LOGIC;
+           memread, memwrite:   out STD_LOGIC;
+           --regdst:              out STD_LOGIC;
+           regwrite:            out STD_LOGIC;
+           alucontrol:          out  STD_LOGIC_VECTOR(3 downto 0));
+    end component;
+    
+begin
+    md: main_decoder port map( op=>op, alusrc=>alusrc, jump=>jump, memread=>memread, memwrite=>memwrite, regwrite=>regwrite, alucontrol=>alucontrol);  --regdst=>regdst
+
+end Behavioral;
---
+++ CU_DPU/CU_DPU.srcs/sources_1/new/main_decoder.vhd
@@ -0,0 +1,56 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+
+entity main_decoder is
+    port(op:                in  STD_LOGIC_VECTOR(7 downto 0);
+       alusrc:              out STD_LOGIC;
+       jump:                out STD_LOGIC;
+       memread, memwrite:   out STD_LOGIC;
+       --regdst:              out STD_LOGIC;
+       regwrite:            out STD_LOGIC;
+       alucontrol:          out  STD_LOGIC_VECTOR(3 downto 0));
+end main_decoder;
+
+architecture Behavioral of main_decoder is
+    signal controls: STD_LOGIC_VECTOR(8 downto 0);
+begin
+      process(op) begin
+        case op is
+          when "00010000" => controls <= "0000110000"; -- And
+          when "00010001" => controls <= "0000110001"; -- Or
+          when "00010010" => controls <= "0000110010"; -- Xor
+          when "00010011" => controls <= "0000110011"; -- Add
+          when "00010100" => controls <= "0000110100"; -- Increment
+          when "00010101" => controls <= "0000110101"; -- Multiply
+          when "00010110" => controls <= "0000110110"; -- Division
+          when "00010111" => controls <= "0000110111"; -- Modulus
+          when "00011000" => controls <= "0000111000"; -- Nand
+          when "00011001" => controls <= "0000111001"; -- Nor
+          when "00011010" => controls <= "0000111010"; -- Not
+          when "00011011" => controls <= "0000111011"; -- Subtract
+          when "00011100" => controls <= "0000111100"; -- Decrement
+          --when "00011101" => controls <= "0000111101"; -- ---
+          when "00011110" => controls <= "0000111110"; -- Exponential
+          when "00011111" => controls <= "0000111111"; -- Set less than
+          when "00100001" => controls <= ""; -- ld
+          when "00100010" => controls <= ""; -- St
+          when "00100011" => controls <= ""; -- li
+          when "00110001" => controls <= ""; -- jmp
+          when "01000001" => controls <= ""; -- mov
+          when others     => controls <= "-----------"; -- illegal op
+        end case;
+      end process;
+    
+      alusrc <= controls(9);
+      jump   <= controls(8);
+      memread   <= controls(7);
+      memwrite   <= controls(6);
+      regwrite <= controls(5);
+      --regdst <= controls(4);
+      alucontrol    <= controls(3 downto 0);
+
+
+
+end Behavioral;
